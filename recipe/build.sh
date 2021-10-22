25a5d4539c5016d969eac9d21ecb70fb9a603ef9
Ryan Volz <ryan.volz@gmail.com>
2021-10-22 16:17:44 -0400

    Add testing and boost-cpp run dependency.

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,8 +10,11 @@ cmake_config_args=(
     -DCMAKE_INSTALL_PREFIX=$PREFIX
     -DLIB_SUFFIX=""
     -DENABLE_DOXYGEN=OFF
+    -DENABLE_TESTING=ON
 )
 
 cmake .. "${cmake_config_args[@]}"
 cmake --build . --config Release -- -j${CPU_COUNT}
 cmake --build . --config Release --target install
+
+ctest --build-config Release --output-on-failure --timeout 120 -j${CPU_COUNT}
